59fbf812752c693f622bda748e9fe42043c664c5
104202994 <158236676+104202994@users.noreply.github.com>
2024-07-01 11:15:26 +1000

    Update trigger_response.sh

--- trigger_response.sh
+++ trigger_response.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Path to the flag file for triggering page reload or action
-FLAG_FILE="/var/www/html/dvwa/config/reload_flag.txt"
+# Email details
+recipient="104202994@student.swin.edu.au"
+sender="104202994@student.swin.edu.au"
+subject="Test Email from Swatch"
+body="This is a test email from Swatch."
 
-# Create/update the flag file
-echo "XSS attack detected" > $FLAG_FILE
+# Set AWS environment variables
+export AWS_CONFIG_FILE=/home/sparsh/.aws/config
+export AWS_ACCESS_KEY_ID=your_access_key
+export AWS_SECRET_ACCESS_KEY=your_secret_key
+
+# Send email using Amazon SES via AWS CLI
+aws ses send-email \
+    --from "$sender" \
+    --to "$recipient" \
+    --subject "$subject" \
+    --text "$body" \
+    --region us-east-1
+
+
