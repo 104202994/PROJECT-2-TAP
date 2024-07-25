1d0455ad308f1a3d23bcbbea24249a431ce79153
NilanshuBasnet <46049944+NilanshuBasnet@users.noreply.github.com>
2024-07-25 17:39:57 +1000

    Send notifications to Slack channel

---
+++ Python/slack_alert.py
@@ -0,0 +1,15 @@
+import requests
+
+SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/'
+
+def send_slack_notification(ip_address):
+    """Send a notification to a Slack channel using the webhook URL."""
+    message = {
+        "text": f"Alert! Potential DoS attack detected and IP address {ip_address} has been blocked."
+    }
+    try:
+        response = requests.post(SLACK_WEBHOOK_URL, json=message)
+        if response.status_code != 200:
+            print(f"Failed to send Slack notification: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending Slack notification: {e}")
